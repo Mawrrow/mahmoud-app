ed50496cecb0ec2facd720315c3d1d9deb77cbee
Malwattar <malwattar@ejada.com>
2026-01-26 14:57:03 +0300

    completed header

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,11 @@
+import Nav from "./Nav";
+
 // src/Header.js
 export default function Header() {
   return (
     <header>
       <img src="/Logo.svg" alt="Little Lemon logo" />
+      <Nav />
     </header>
   );
 }
